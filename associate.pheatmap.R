ffb2470998d4307c65f28c10ac308ab0eb99746f
Ryohei SHIBATA <shibataryohei@gmail.com>
2021-04-20 06:08:09 +0900

    !!!

---
+++ associate.pheatmap.R
@@ -0,0 +1,29 @@
+require(microbiome)
+require(tidyverse)
+require(pheatmap)
+
+associate.pheatmap <- function(df1, df2){
+  intersect(rownames(df1),
+            rownames(df2)) -> mutualid
+  
+  microbiome::associate(df1[mutualid,],
+                        df2[mutualid, ]) %>% 
+    as.tibble %>% 
+    dplyr::rename(Q.value = p.adj) -> associate_tbw
+  
+  associate_tbw %>% 
+    dplyr::select(-Q.value) %>% 
+    spread(X2, Correlation) %>% 
+    c2r("X1") -> correlation_df
+  
+  associate_tbw %>% 
+    dplyr::select(-Correlation) %>% 
+    mutate(Q.value = addsd(Q.value)) %>% 
+    spread(X2, Q.value) %>% 
+    c2r("X1") -> stars_df
+  
+  stars_df[is.na(stars_df)] <- " "
+  
+  list(associate_tbw,
+       correlation_df,
+       stars_df)}
