52250c518d8ad5df20e427beb5520433c4cc064d
Ryohei SHIBATA <shibataryohei@gmail.com>
2021-10-26 07:53:34 +0900

    ???

--- Install_Packages.R
+++ Install_Packages.R
@@ -17,7 +17,8 @@ Data <-
   c("readxl","forcats","magrittr") 
 
 Statistics <-
-  c("lme4", "exactRankTests", "tableone", "qvalue")
+  c("lme4", "exactRankTests", "tableone", "qvalue",
+    "rstatix", "qvalue", "sjPlot", "survival", "randomForest")
 
 Knit <- c("bindrcpp", "carData", "car", "sandwich", "RcmdrMisc", "effects",
           "Rcmdr")
@@ -25,16 +26,25 @@ Knit <- c("bindrcpp", "carData", "car", "sandwich", "RcmdrMisc", "effects",
 Packages <- c(Document, Graphic, Data, Statistics, Knit)
 install.packages(Packages)
 
+install.pa
+
 # devtools
 install.packages("devtools")
 devtools::install_github("vqv/ggbiplot")
+devtools::install_github("sjPlot/devel")
 
 # phyloseq
-if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-
+install.packages('BiocManager')
+BiocManager::install("GenomeInfoDb")
+BiocManager::install("Rhdf5lib")
 BiocManager::install("phyloseq")
 
+library(phyloseq)
 
-# 2019/07/31
-install.packages('BiocManager')
+# Error
+install.packages("webshot")
+
+# Add-hoc
+install.packages("devtools")
+devtools::install_github("erhard-lab/lfc")
+library(lfc)
